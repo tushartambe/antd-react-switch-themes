fbf23f8c54e96a4f4afddc679dfad807a841d34e
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2021-03-26 11:55:05 +0530

    Initail Commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { Switch, Typography } from 'antd';
 import './App.css';
+import { useThemeSwitcher } from "react-css-theme-switcher";
+import { useState } from 'react';
+
+const { Title } = Typography;
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const { switcher, themes } = useThemeSwitcher();
+
+  const switchTheme = (isDarkMode) => {
+    setIsDarkMode(isDarkMode);
+    switcher({ theme: isDarkMode ? themes.dark : themes.light });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title level={4}>Switching between themes in antd</Title>
+      <Switch
+        checkedChildren={"Dark Theme"}
+        unCheckedChildren={"Light Theme"}
+        checked={isDarkMode}
+        onChange={switchTheme}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const themes = {
+  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
+  light: `${process.env.PUBLIC_URL}/light-theme.css`,
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeSwitcherProvider themeMap={themes} defaultTheme="light">
+      <App />
+    </ThemeSwitcherProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
